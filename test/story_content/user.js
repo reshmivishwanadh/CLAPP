5b2c186a758e26fcaff0577a8e76a5fb27ee229a
DESKTOP-HNQAEFT\admin <reshmi.esc@gmail.com>
2018-07-23 04:02:39 +0530

    mv5

--- test/story_content/user.js
+++ test/story_content/user.js
@@ -17,38 +17,45 @@ var name= "hii";
 var scr ="299";
   var checkStatus = function() {
     if (navigator.onLine) {
-          $.ajax({
-       type: 'GET',
-       url: 'http://www.creatnlrn.com/api/test/user/'+name+'/'+scr+'/',
-       //data: {'name':name,'score' : test},
-       cache: false,
-       success: function(response){
-        window.location.href = "../index.html";
-      },
-       error: function (data) {
 
-              if(data.length > 0)
-                        {
-                            for(key in data)
-                            {
-                                var tmp = data[key];
-                                alert(tmp.title);
-                            }
-                        }
-                        else
-                        {
-                            alert("no result");
-                        }
+              name="maya";
+              scr="200";
+              $.ajax({
+                         type: 'GET',
+                         url: 'http://www.creatnlrn.com/api/test/user/'+name+'/'+scr+'/',
+                         //data: {'name':'abc','score' : '80'},
+                         //dataType: 'json',
+                         cache: false,
+                         success: function(response){
+                          alert("done");
+                          },
+                          error: function (data) {
+                            //alert("error"+response.responseText);
+                                if(data.length > 0)
+                                          {
+                                              for(key in data)
+                                              {
+                                                  var tmp = data[key];
+                                                  alert(tmp.title);
+                                              }
+                                          }
+                                          else
+                                          {
+                                              alert("no result");
+                                          }
 
 
-       }
-      });
+                          }
+                    });
+ 
+
+
       sessionStorage.clear();    
     } else {
       alert("Please connect your system to internet");
     }
   }
-  checkStatus()
+  checkStatus();
 
 }
 
